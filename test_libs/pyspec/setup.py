3f6d6535ada35fd648f7ed6fad084c080f8af037
protolambda <proto@protolambda.com>
2019-03-28 22:04:51 +0800

    Fix pyspec package structure

--- test_libs/pyspec/setup.py
+++ test_libs/pyspec/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyspec',
     version='1.0',
-    packages=['eth2'],
+    packages=['eth2.debug', 'eth2.phase0', 'eth2.utils'],
     install_requires=[
         "eth-utils>=1.3.0,<2",
         "eth-typing>=2.1.0,<3.0.0",
