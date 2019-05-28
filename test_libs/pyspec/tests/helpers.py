00ffaf4d71e6f26667f37616418618cef7fc0559
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-05-28 12:37:41 +0800

    Fix ssz path

--- test_libs/pyspec/tests/helpers.py
+++ test_libs/pyspec/tests/helpers.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 from py_ecc import bls
 
 import eth2spec.phase0.spec as spec
-from eth2spec.utils.minimal_ssz import signing_root
+from eth2spec.utils.ssz.ssz_impl import signing_root
 from eth2spec.phase0.spec import (
     # constants
     ZERO_HASH,
