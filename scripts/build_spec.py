7d2f0a9dc0df1c69fc8178160325043b06cbb92c
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-06-18 14:07:05 -0600

    clean up

--- scripts/build_spec.py
+++ scripts/build_spec.py
@@ -132,17 +132,11 @@ def objects_to_spec(functions: Dict[str, str],
                 f"    def __init__(self, _x: {value}) -> None:\n"
                 f"        ...\n"
                 if value.startswith("uint")
-                # else ""
-                # else f"{key} = {value}\n"
                 else f"class {key}({value}):\n    pass\n"
                 for key, value in custom_types.items()
             ]
         )
     )
-    # new_type_definitions += '\n'.join(['Bytes%s = BytesN[%s]' % (n, n) for n in byte_types])
-    # new_type_definitions += '\n' + '\n'.join(['Hash = Bytes32', 'BLSPubkey = Bytes48', 'BLSSignature = Bytes96'])
-    # new_type_definitions += \
-    #     '\n' + '\n'.join(['''%s = NewType('%s', %s)''' % (key, key, value) for key, value in new_types.items()])
     functions_spec = '\n\n'.join(functions.values())
     constants_spec = '\n'.join(map(lambda x: '%s = %s' % (x, constants[x]), constants))
     ssz_objects_instantiation_spec = '\n\n'.join(ssz_objects.values())
