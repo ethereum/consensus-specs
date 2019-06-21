5fc733bef69c2e69f3620bb83e389461e89dd267
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-06-21 11:02:38 -0600

    Comment out `test_transfer`

--- test_libs/pyspec/eth2spec/test/sanity/test_blocks.py
+++ test_libs/pyspec/eth2spec/test/sanity/test_blocks.py
@@ -1,13 +1,11 @@
 from copy import deepcopy
 from typing import List
 
-import pytest
-
 from eth2spec.utils.ssz.ssz_impl import signing_root
 from eth2spec.utils.bls import bls_sign
 
 from eth2spec.test.helpers.state import get_balance, state_transition_and_sign_block
-from eth2spec.test.helpers.transfers import get_valid_transfer
+# from eth2spec.test.helpers.transfers import get_valid_transfer
 from eth2spec.test.helpers.block import build_empty_block_for_next_slot, sign_block
 from eth2spec.test.helpers.keys import privkeys, pubkeys
 from eth2spec.test.helpers.attester_slashings import get_valid_attester_slashing
@@ -311,39 +309,38 @@ def test_voluntary_exit(spec, state):
     assert state.validator_registry[validator_index].exit_epoch < spec.FAR_FUTURE_EPOCH
 
 
-@with_all_phases
-@spec_state_test
-@pytest.mark.skip("Transfer is disabled for now")
-def test_transfer(spec, state):
-    # overwrite default 0 to test
-    spec.MAX_TRANSFERS = 1
+# @with_all_phases
+# @spec_state_test
+# def test_transfer(spec, state):
+#     # overwrite default 0 to test
+#     spec.MAX_TRANSFERS = 1
 
-    sender_index = spec.get_active_validator_indices(state, spec.get_current_epoch(state))[-1]
-    amount = get_balance(state, sender_index)
+#     sender_index = spec.get_active_validator_indices(state, spec.get_current_epoch(state))[-1]
+#     amount = get_balance(state, sender_index)
 
-    transfer = get_valid_transfer(spec, state, state.slot + 1, sender_index, amount, signed=True)
-    recipient_index = transfer.recipient
-    pre_transfer_recipient_balance = get_balance(state, recipient_index)
+#     transfer = get_valid_transfer(spec, state, state.slot + 1, sender_index, amount, signed=True)
+#     recipient_index = transfer.recipient
+#     pre_transfer_recipient_balance = get_balance(state, recipient_index)
 
-    # un-activate so validator can transfer
-    state.validator_registry[sender_index].activation_eligibility_epoch = spec.FAR_FUTURE_EPOCH
+#     # un-activate so validator can transfer
+#     state.validator_registry[sender_index].activation_eligibility_epoch = spec.FAR_FUTURE_EPOCH
 
-    yield 'pre', state
+#     yield 'pre', state
 
-    # Add to state via block transition
-    block = build_empty_block_for_next_slot(spec, state)
-    block.body.transfers.append(transfer)
-    sign_block(spec, state, block)
+#     # Add to state via block transition
+#     block = build_empty_block_for_next_slot(spec, state)
+#     block.body.transfers.append(transfer)
+#     sign_block(spec, state, block)
 
-    state_transition_and_sign_block(spec, state, block)
+#     state_transition_and_sign_block(spec, state, block)
 
-    yield 'blocks', [block], List[spec.BeaconBlock]
-    yield 'post', state
+#     yield 'blocks', [block], List[spec.BeaconBlock]
+#     yield 'post', state
 
-    sender_balance = get_balance(state, sender_index)
-    recipient_balance = get_balance(state, recipient_index)
-    assert sender_balance == 0
-    assert recipient_balance == pre_transfer_recipient_balance + amount
+#     sender_balance = get_balance(state, sender_index)
+#     recipient_balance = get_balance(state, recipient_index)
+#     assert sender_balance == 0
+#     assert recipient_balance == pre_transfer_recipient_balance + amount
 
 
 @with_all_phases
