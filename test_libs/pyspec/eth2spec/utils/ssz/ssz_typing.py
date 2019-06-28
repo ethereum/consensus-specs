128bbbc665c8eabe10640c019ccb0765c5be8f11
protolambda <proto@protolambda.com>
2019-06-28 17:27:59 +0200

    fix slicing, and support partial slice bounds

--- test_libs/pyspec/eth2spec/utils/ssz/ssz_typing.py
+++ test_libs/pyspec/eth2spec/utils/ssz/ssz_typing.py
@@ -325,7 +325,7 @@ class BaseList(list, Elements):
 
     def __setitem__(self, k, v):
         if type(k) == slice:
-            if k.start < 0 or k.stop > len(self):
+            if (k.start is not None and k.start < 0) or (k.stop is not None and k.stop > len(self)):
                 raise IndexError(f"cannot set item in type {self.__class__}"
                                  f" at out of bounds slice {k} (to {v}, bound: {len(self)})")
             super().__setitem__(k, [coerce_type_maybe(x, self.__class__.elem_type) for x in v])
