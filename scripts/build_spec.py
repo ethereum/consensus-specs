b259d3518b28622b2627a4b23932d790ff8d652c
Danny Ryan <dannyjryan@gmail.com>
2019-09-30 11:34:28 +0900

    move assert to get_beacon_proposer_index

--- scripts/build_spec.py
+++ scripts/build_spec.py
@@ -37,7 +37,7 @@ from eth2spec.utils.bls import (
 from eth2spec.utils.hash_function import hash
 '''
 PHASE1_IMPORTS = '''from typing import (
-    Any, Dict, Optional, Set, Sequence, MutableSequence, NewType, Tuple, Union,
+    Any, Dict, Set, Sequence, MutableSequence, NewType, Tuple, Union,
 )
 from math import (
     log2,
