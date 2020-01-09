f810e6b9c2b2eeefe4b37dd9bdf08cf731646bbb
protolambda <proto@protolambda.com>
2020-01-09 15:16:46 +0100

    update to python v3.8

--- test_libs/pyspec/setup.py
+++ test_libs/pyspec/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyspec',
     packages=find_packages(),
+    python_requires=">=3.8, <4",
     tests_require=["pytest"],
     install_requires=[
         "eth-utils>=1.3.0,<2",
