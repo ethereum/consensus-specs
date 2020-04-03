3cb429cccc3cfadb73ed38be2bcddad5bd7f8216
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-04-04 01:38:20 +0800

    Add mypy stubs for third party packages

---
+++ mypy_stubs/eth2spec/utils/ssz/ssz_typing.pyi
@@ -0,0 +1,5 @@
+from remerkleable.basic import bit as bit, boolean as boolean, byte as byte, uint as uint, uint128 as uint128, uint16 as uint16, uint256 as uint256, uint32 as uint32, uint64 as uint64, uint8 as uint8
+from remerkleable.bitfields import Bitlist as Bitlist, Bitvector as Bitvector
+from remerkleable.byte_arrays import ByteList as ByteList, ByteVector as ByteVector, Bytes1 as Bytes1, Bytes32 as Bytes32, Bytes4 as Bytes4, Bytes48 as Bytes48, Bytes8 as Bytes8, Bytes96 as Bytes96
+from remerkleable.complex import Container as Container, List as List, Vector as Vector
+from remerkleable.core import BasicView as BasicView, TypeDef as TypeDef, View as View
