2739718922b1facb3464c48c4433c757531b4298
Alex Beregszaszi <alex@rtfs.hu>
2020-08-13 21:14:46 +0100

    Remove duplicated Natspec from the implementation (#36)
    Also update binary output due to metadata change.

--- deposit_contract.sol
+++ deposit_contract.sol
@@ -77,8 +77,6 @@ contract DepositContract is IDepositContract, ERC165 {
             zero_hashes[height + 1] = sha256(abi.encodePacked(zero_hashes[height], zero_hashes[height]));
     }
 
-    /// @notice Query the current deposit root hash.
-    /// @return The deposit root hash.
     function get_deposit_root() override external view returns (bytes32) {
         bytes32 node;
         uint size = deposit_count;
@@ -96,18 +94,10 @@ contract DepositContract is IDepositContract, ERC165 {
         ));
     }
 
-    /// @notice Query the current deposit count.
-    /// @return The deposit count encoded as a little endian 64-bit number.
     function get_deposit_count() override external view returns (bytes memory) {
         return to_little_endian_64(uint64(deposit_count));
     }
 
-    /// @notice Submit a Phase 0 DepositData object.
-    /// @param pubkey A BLS12-381 public key.
-    /// @param withdrawal_credentials Commitment to a public key for withdrawals.
-    /// @param signature A BLS12-381 signature.
-    /// @param deposit_data_root The SHA-256 hash of the SSZ-encoded DepositData object.
-    /// Used as a protection against malformed input.
     function deposit(
         bytes calldata pubkey,
         bytes calldata withdrawal_credentials,
