fd4e7dde943c2805b2c09657a838ce0e860cfeef
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-09-07 13:34:23 +0800

    Fix `disable_process_reveal_deadlines` decorator: should have set it back to the spec function for other test cases afterwards

--- tests/core/pyspec/eth2spec/test/context.py
+++ tests/core/pyspec/eth2spec/test/context.py
@@ -264,6 +264,24 @@ def bls_switch(fn):
     return entry
 
 
+def disable_process_reveal_deadlines(fn):
+    """
+    Decorator to make a function execute with `process_reveal_deadlines` OFF.
+    This is for testing long-range epochs transition without considering the reveal-deadline slashing effect.
+    """
+    def entry(*args, spec: Spec, **kw):
+        if hasattr(spec, 'process_reveal_deadlines'):
+            old_state = spec.process_reveal_deadlines
+            spec.process_reveal_deadlines = lambda state: None
+
+        yield from fn(*args, spec=spec, **kw)
+
+        if hasattr(spec, 'process_reveal_deadlines'):
+            spec.process_reveal_deadlines = old_state
+
+    return with_meta_tags({'reveal_deadlines_setting': 1})(entry)
+
+
 def with_all_phases(fn):
     """
     A decorator for running a test with every phase
@@ -319,14 +337,6 @@ def with_phases(phases, other_phases=None):
             return ret
         return wrapper
     return decorator
-
-
-def disable_process_reveal_deadlines(fn):
-    def entry(*args, spec: Spec, **kw):
-        if hasattr(spec, 'process_reveal_deadlines'):
-            spec.process_reveal_deadlines = lambda state: None
-        return fn(*args, spec=spec, **kw)
-    return with_meta_tags({'reveal_deadlines_setting': 1})(entry)
 
 
 def with_configs(configs, reason=None):
