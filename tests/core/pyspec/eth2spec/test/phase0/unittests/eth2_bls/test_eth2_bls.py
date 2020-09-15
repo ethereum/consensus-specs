b32f35034236d432c33d48348a48b0246292597f
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-09-16 01:04:25 +0800

    Add sample test

---
+++ tests/core/pyspec/eth2spec/test/phase0/unittests/eth2_bls/test_eth2_bls.py
@@ -0,0 +1,15 @@
+from eth2spec.test.context import with_all_phases, spec_state_test
+
+
+@with_all_phases
+@spec_state_test
+def test_eth2_verify(spec, state):
+    PK = spec.G1_INFINATY_POINT
+    signature = spec.G2_INFINATY_POINT
+    message = b'hello'
+
+    # Valid in IETF BLS v3
+    # TODO: will be invalid when we update it to v4
+    assert spec.bls.Verify(PK, message, signature)
+    # Valid in Eth2 spec
+    assert spec.Eth2Verify(PK, message, signature)
