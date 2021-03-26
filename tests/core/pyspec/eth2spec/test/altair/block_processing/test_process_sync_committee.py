582965fd0bdc03c49af564f0c417c6ab30e7545c
Barnabé Monnot <b@barnabemonnot.com>
2021-03-26 18:57:02 +0800

    Deprecate PROPOSER_REWARD_QUOTIENT from tests

--- tests/core/pyspec/eth2spec/test/altair/block_processing/test_process_sync_committee.py
+++ tests/core/pyspec/eth2spec/test/altair/block_processing/test_process_sync_committee.py
@@ -137,7 +137,7 @@ def compute_sync_committee_proposer_reward(spec, state, committee, committee_bit
         inclusion_reward = compute_sync_committee_inclusion_reward(
             spec, state, index, committee, committee_bits,
         )
-        proposer_denominator = (spec.WEIGHT_DENOMINATOR - spec.PROPOSER_WEIGHT) * spec.PROPOSER_REWARD_QUOTIENT
+        proposer_reward_denominator = (spec.WEIGHT_DENOMINATOR - spec.PROPOSER_WEIGHT) * spec.WEIGHT_DENOMINATOR // spec.PROPOSER_WEIGHT
         proposer_reward += spec.Gwei((inclusion_reward * spec.WEIGHT_DENOMINATOR) // proposer_denominator)
     return proposer_reward
 
