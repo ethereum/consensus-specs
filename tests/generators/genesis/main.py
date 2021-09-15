829c6e732597fe48ded17bdb656d806d9eb2e0e5
Dmitrii Shmatko <leodex23@gmail.com>
2021-09-15 18:27:20 +0300

    Enable genesis client tests for MERGE

--- tests/generators/genesis/main.py
+++ tests/generators/genesis/main.py
@@ -1,5 +1,5 @@
 from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators
-from eth2spec.test.helpers.constants import PHASE0, ALTAIR
+from eth2spec.test.helpers.constants import PHASE0, ALTAIR, MERGE
 
 
 if __name__ == "__main__":
@@ -8,9 +8,12 @@ if __name__ == "__main__":
         'validity',
     ]}
     altair_mods = phase_0_mods
+    # we have new unconditional lines in `initialize_beacon_state_from_eth1` and we want to test it
+    merge_mods = altair_mods
     all_mods = {
         PHASE0: phase_0_mods,
         ALTAIR: altair_mods,
+        MERGE: merge_mods,
     }
 
     run_state_test_generators(runner_name="genesis", all_mods=all_mods)
