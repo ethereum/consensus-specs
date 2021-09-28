939e6c7e8eab56a8cce197b5d25218026198b0c9
Etan Kissling <etan@status.im>
2021-09-28 23:30:29 +0200

    run tests against future forks by default
    Some tests are currently restricted to a single phase using @with_phases
    even though they could likely run unchanged in later phases. This patch
    changes the default for such tests to also run in later phases. If the
    beacon chain changes enough in later phases to break these tests, this
    highlights that the tests need to be adjusted or extended accordingly.

--- tests/core/pyspec/eth2spec/test/altair/merkle/test_single_proof.py
+++ tests/core/pyspec/eth2spec/test/altair/merkle/test_single_proof.py
@@ -1,12 +1,11 @@
 from eth2spec.test.context import (
     spec_state_test,
-    with_phases,
+    with_altair_and_later,
 )
-from eth2spec.test.helpers.constants import ALTAIR
 from eth2spec.test.helpers.merkle import build_proof
 
 
-@with_phases([ALTAIR])
+@with_altair_and_later
 @spec_state_test
 def test_next_sync_committee_merkle_proof(spec, state):
     yield "state", state
@@ -25,7 +24,7 @@ def test_next_sync_committee_merkle_proof(spec, state):
     )
 
 
-@with_phases([ALTAIR])
+@with_altair_and_later
 @spec_state_test
 def test_finality_root_merkle_proof(spec, state):
     yield "state", state
--- tests/generators/merkle/main.py
+++ tests/generators/merkle/main.py
@@ -1,4 +1,4 @@
-from eth2spec.test.helpers.constants import ALTAIR
+from eth2spec.test.helpers.constants import ALTAIR, MERGE
 from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators
 
 
@@ -6,9 +6,11 @@ if __name__ == "__main__":
     altair_mods = {key: 'eth2spec.test.altair.merkle.test_' + key for key in [
         'single_proof',
     ]}
+    merge_mods = altair_mods
 
     all_mods = {
-        ALTAIR: altair_mods
+        ALTAIR: altair_mods,
+        MERGE: merge_mods,
     }
 
     run_state_test_generators(runner_name="merkle", all_mods=all_mods)
