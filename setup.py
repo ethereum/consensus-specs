79f1f16adf4fc7f1c621a8bbe1471dd06430ccb3
Hsiao-Wei Wang <hsiaowei.eth@gmail.com>
2021-11-16 22:08:00 +0800

    Clean `PowBlock.difficulty` leftover

--- setup.py
+++ setup.py
@@ -511,7 +511,7 @@ ExecutionState = Any
 
 
 def get_pow_block(hash: Bytes32) -> Optional[PowBlock]:
-    return PowBlock(block_hash=hash, parent_hash=Bytes32(), total_difficulty=uint256(0), difficulty=uint256(0))
+    return PowBlock(block_hash=hash, parent_hash=Bytes32(), total_difficulty=uint256(0))
 
 
 def get_execution_state(execution_state_root: Bytes32) -> ExecutionState:
