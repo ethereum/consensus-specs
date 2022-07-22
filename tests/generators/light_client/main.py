176a5d227a105d0cf3a2d09a809a1514710d6bf6
Hsiao-Wei Wang <hsiaowei.eth@gmail.com>
2022-07-22 21:34:09 +0800

    Move `merkle/single_proof` tests to `light_client/single_merkle_proof`

--- tests/generators/light_client/main.py
+++ tests/generators/light_client/main.py
@@ -4,6 +4,7 @@ from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators
 
 if __name__ == "__main__":
     altair_mods = {key: 'eth2spec.test.altair.light_client.test_' + key for key in [
+        'single_merkle_proof',
         'sync',
         'update_ranking',
     ]}
