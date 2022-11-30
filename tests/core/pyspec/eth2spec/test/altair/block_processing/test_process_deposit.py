a66cf79c9c0b702a25d9ba0e93af3c2f6e3e1c5b
Hsiao-Wei Wang <hsiaowei.eth@gmail.com>
2022-11-30 16:35:41 +0800

    Clean up Altair and Bellatrix `process_deposit` tests

--- tests/core/pyspec/eth2spec/test/altair/block_processing/test_process_deposit.py
+++ tests/core/pyspec/eth2spec/test/altair/block_processing/test_process_deposit.py
@@ -1,37 +1,40 @@
 from eth2spec.test.context import (
     spec_state_test,
     always_bls,
-    with_bellatrix_and_later,
+    with_altair_and_later,
+    with_phases,
 )
+from eth2spec.test.helpers.constants import (
+    ALTAIR,
+)
+
+
 from eth2spec.test.helpers.deposits import (
     run_deposit_processing_with_specific_fork_version,
 )
 
 
-@with_bellatrix_and_later
+@with_phases([ALTAIR])
 @spec_state_test
 @always_bls
-def test_ineffective_deposit_with_previous_fork_version(spec, state):
-    # NOTE: it was effective in Altair.
+def test_effective_deposit_with_previous_fork_version(spec, state):
     assert state.fork.previous_version != state.fork.current_version
 
     yield from run_deposit_processing_with_specific_fork_version(
         spec,
         state,
         fork_version=state.fork.previous_version,
-        effective=False,
+        effective=True,
     )
 
 
-@with_bellatrix_and_later
+@with_altair_and_later
 @spec_state_test
 @always_bls
-def test_effective_deposit_with_genesis_fork_version(spec, state):
-    assert spec.config.GENESIS_FORK_VERSION not in (state.fork.previous_version, state.fork.current_version)
-
+def test_ineffective_deposit_with_bad_fork_version_and(spec, state):
     yield from run_deposit_processing_with_specific_fork_version(
         spec,
         state,
-        fork_version=spec.config.GENESIS_FORK_VERSION,
-        effective=True,
+        fork_version=spec.Version('0xAaBbCcDd'),
+        effective=False,
     )
