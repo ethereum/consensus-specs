6e9dad10f75b0df096430c02c33dd7900fd746a5
Mikhail Kalinin <noblesse.knight@gmail.com>
2023-04-14 12:06:14 +0600

    Add attestation and sync_aggregate tests

--- tests/core/pyspec/eth2spec/test/phase0/block_processing/test_process_attestation.py
+++ tests/core/pyspec/eth2spec/test/phase0/block_processing/test_process_attestation.py
@@ -6,6 +6,7 @@ from eth2spec.test.context import (
     low_balances,
     with_custom_state,
     single_phase,
+    with_altair_and_later,
 )
 from eth2spec.test.helpers.attestations import (
     run_attestation_processing,
@@ -19,6 +20,7 @@ from eth2spec.test.helpers.state import (
     transition_to_slot_via_block,
 )
 from eth2spec.utils.ssz.ssz_typing import Bitlist
+from eth2spec.test.helpers.block import build_empty_block
 
 
 @with_all_phases
@@ -564,3 +566,23 @@ def test_invalid_incorrect_target_included_after_epoch_delay(spec, state):
     sign_attestation(spec, state, attestation)
 
     yield from run_attestation_processing(spec, state, attestation, valid=False)
+
+
+@with_altair_and_later
+@spec_state_test
+def test_slashed_proposer_rewarded_for_attestation_inclusion(spec, state):
+    attestation = get_valid_attestation(spec, state, signed=True)
+    next_slots(spec, state, spec.MIN_ATTESTATION_INCLUSION_DELAY)
+
+    # Process block header as proposer index is read from the state since Deneb
+    block = build_empty_block(spec, state)
+    spec.process_block_header(state, block)
+
+    # Slash proposer
+    state.validators[block.proposer_index].slashed = True
+    pre_state_proposer_balance = state.balances[block.proposer_index]
+
+    yield from run_attestation_processing(spec, state, attestation)
+
+    # Check proposer gets rewarded
+    assert state.balances[block.proposer_index] > pre_state_proposer_balance
