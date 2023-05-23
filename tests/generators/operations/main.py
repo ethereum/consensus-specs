fc45220a7d1ee5bc75be4c5a0a834512f812f8f1
Hsiao-Wei Wang <hsiaowei.eth@gmail.com>
2023-05-23 15:38:54 +0800

    Move old Deneb sanity tests to block_processing (operations) tests

--- tests/generators/operations/main.py
+++ tests/generators/operations/main.py
@@ -36,7 +36,10 @@ if __name__ == "__main__":
     ]}
     capella_mods = combine_mods(_new_capella_mods, bellatrix_mods)
 
-    deneb_mods = capella_mods
+    _new_deneb_mods = {key: 'eth2spec.test.deneb.block_processing.test_process_' + key for key in [
+        'execution_payload',
+    ]}
+    deneb_mods = combine_mods(_new_deneb_mods, capella_mods)
 
     _new_eip6110_mods = {key: 'eth2spec.test.eip6110.block_processing.test_process_' + key for key in [
         'deposit_receipt',
