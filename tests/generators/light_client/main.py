e1a55238f2576aa4829cdf32357d530eb7fa0301
Hsiao-Wei Wang <hsiaowei.eth@gmail.com>
2023-06-02 00:06:47 +0800

    Delete eip6110_mods

--- tests/generators/light_client/main.py
+++ tests/generators/light_client/main.py
@@ -15,14 +15,12 @@ if __name__ == "__main__":
     ]}
     capella_mods = combine_mods(_new_capella_mods, bellatrix_mods)
     deneb_mods = capella_mods
-    eip6110_mods = deneb_mods
 
     all_mods = {
         ALTAIR: altair_mods,
         BELLATRIX: bellatrix_mods,
         CAPELLA: capella_mods,
         DENEB: deneb_mods,
-        EIP6110: eip6110_mods,
     }
 
     run_state_test_generators(runner_name="light_client", all_mods=all_mods)
