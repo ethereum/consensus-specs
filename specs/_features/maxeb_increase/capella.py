31e056e0448b5b1c4aefb84e9a722e6ed2cd4f72
Michael Neuder <michael.neuder@gmail.com>
2023-07-06 15:12:12 -0400

    rm decrease_balance

--- specs/_features/maxeb_increase/capella.py
+++ specs/_features/maxeb_increase/capella.py
@@ -3701,7 +3701,6 @@ def get_expected_withdrawals(state: BeaconState) -> Sequence[Withdrawal]:
                 address=ExecutionAddress(validator.withdrawal_credentials[12:]),
                 amount=withdrawable_balance,
             ))
-            decrease_balance(state, withdrawal.index, withdrawable_balance)
             withdrawal_index += WithdrawalIndex(1)
             consumed += 1
 
