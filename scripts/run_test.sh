548a0f2cc12690097d0f630de366cf618f3a9e44
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2023-08-07 22:43:07 +0200

    📝 add documentation + helps

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,17 +1,20 @@
 # /bin/sh
 
-# constants
+# Run 'consensus-specs' tests from a container instance
+# Be sure to launch Docker before running this script
+
+# Constants
 ALL_EXECUTABLE_SPECS=("phase0" "altair" "bellatrix" "capella" "deneb" "eip6110" "whisk")
 TEST_PRESET_TYPE=minimal
 WORKDIR="//consensus-specs//tests//core//pyspec"
 
 COV_HTML_OUT=.htmlcov
 
-# default flag value
+# Default flag values
 version=latest
 number_of_core=4
 
-# generate coverage scope
+# Generates coverage scope
 name="eth2spec"
 coverage_scope=()
 for spec in "${ALL_EXECUTABLE_SPECS[@]}"
@@ -19,13 +22,24 @@ do
     coverage_scope+=("--cov=${name}.${spec}.${TEST_PRESET_TYPE}")
 done
 
-# Parse flags
-while getopts v:n: flag
+display_help() {
+  echo "Run 'consensus-specs' tests from a container instance."
+  echo "Be sure to launch Docker before running this script."
+  echo
+  echo "Syntax: run_test [-v TAG | -n NUMBER_OF_CORE | -h HELP]"
+  echo "-v      Specify the image tag for consensus-specs."
+  echo "-n      Specify the number of core to run tests."
+  echo "-h      Get helps."
+}
+
+# Parse provided flags
+while getopts v:n:h flag
 do
     case "${flag}" in
-        v) version=${OPTARG};;
-        n) number_of_core=${OPTARG};;
-        \?) echo "not valid -$OPTARG:${OPTARG}";;
+        v) version=$OPTARG;;
+        n) number_of_core=$OPTARG;;
+        h) display_help && $SHELL;;
+        \?) echo "not valid -$OPTARG" && $SHELL;;
     esac
 done
 
@@ -34,12 +48,13 @@ get_container_name() {
   echo $(docker ps -a -q --filter ancestor="consensus-specs:$version" --filter status="exited" --format="{{.ID}}")
 }
 
-# Stop and remove all container that use the `consensus-specs:$version` image
+# Stop and remove all exited container that use the `consensus-specs:$version` image
 cleanup() {
-  echo "Stop and remove current container"
+  echo "Stop and remove non running containers."
   docker rm $(docker stop $(get_container_name))
 }
 
+# Copy the coverage report from the container to the local
 copy_coverage_report() {
   docker cp $(get_container_name):$WORKDIR/$COV_HTML_OUT ./$COV_HTML_OUT
 }
@@ -55,5 +70,4 @@ $(copy_coverage_report)
 #  the script. It helps user to limit container instances.
 trap cleanup EXIT
 
-# for testing purpose
 $SHELL
