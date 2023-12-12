ec0d19cdebbe23348cd550ecf2abc7a728521368
Mikhail Kalinin <noblesse.knight@gmail.com>
2023-12-12 17:45:17 +0600

    Implement basic version of SM link generator

---
+++ tests/generators/fork_choice_generated/main.py
@@ -0,0 +1,17 @@
+from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators, combine_mods
+from eth2spec.test.helpers.constants import ALTAIR, BELLATRIX, CAPELLA, DENEB, EIP6110
+
+
+if __name__ == "__main__":
+    generated_modes = {key: 'eth2spec.test.phase0.fork_choice.test_' + key for key in [
+        'generated',
+    ]}
+
+    fork_choice_compliance_testing_modes = {
+        ALTAIR: generated_modes,
+        BELLATRIX: generated_modes,
+        CAPELLA: generated_modes,
+        DENEB: generated_modes
+    }
+
+    run_state_test_generators(runner_name="fork_choice_generated", all_mods=fork_choice_compliance_testing_modes)
