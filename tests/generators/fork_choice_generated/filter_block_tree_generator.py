80968610976e01cc98552dfa73c857470f0019e1
Alex Vlasov <avv49@mail.ru>
2024-04-02 03:13:37 +0400

    Modified `filter_block_tree_generator.py`: added an option to loa pre-generated MiniZinc instances.

--- tests/generators/fork_choice_generated/filter_block_tree_generator.py
+++ tests/generators/fork_choice_generated/filter_block_tree_generator.py
@@ -8,9 +8,18 @@ from eth2spec.utils import bls
 from eth2spec.test.helpers.typing import SpecForkName, PresetBaseName
 from minizinc import Instance, Model, Solver
 import random
+from ruamel.yaml import YAML
+
 
 BLS_ACTIVE = False
 GENERATOR_NAME = 'filter_block_tree'
+
+
+def _load_model_solutions(instance_path: str):
+    yaml = YAML(typ='safe')
+    solutions = yaml.load(open(instance_path, 'r'))
+    print('solutions', solutions)
+    return solutions
 
 
 def _import_test_fn():
@@ -23,7 +32,8 @@ def _find_model_solutions(anchor_epoch: int,
                           store_justified_epoch_equal_zero: bool,
                           block_voting_source_epoch_equal_store_justified_epoch: bool,
                           block_voting_source_epoch_plus_two_greater_or_equal_current_epoch: bool,
-                          block_is_leaf: bool) -> []:
+                          block_is_leaf: bool,
+                          nr_solutions: int=5) -> []:
     block_cover3 = Model('./model/minizinc/Block_cover3.mzn')
     solver = Solver.lookup("gecode")
     instance = Instance(solver, block_cover3)
@@ -33,7 +43,7 @@ def _find_model_solutions(anchor_epoch: int,
     instance['block_vse_plus_two_ge_curr_e'] = block_voting_source_epoch_plus_two_greater_or_equal_current_epoch
     instance['block_is_leaf'] = block_is_leaf
 
-    result = instance.solve(nr_solutions=5)
+    result = instance.solve(nr_solutions=nr_solutions)
 
     output = []
     for s in result.solution:
@@ -55,11 +65,39 @@ def _find_model_solutions(anchor_epoch: int,
     return output
 
 
+def _generate_model_solutions(anchor_epoch: int, nr_solutions: int=5):
+    solutions = []
+
+    for store_je_eq_zero in [True, False]:
+        for block_vse_eq_store_je in [True, False]:
+            for block_vse_plus_two_ge_curr_e in [True, False]:
+                for block_is_leaf in [True, False]:
+                    if store_je_eq_zero and not block_vse_eq_store_je:
+                        continue
+                    results = _find_model_solutions(anchor_epoch=0 if store_je_eq_zero else anchor_epoch,
+                                                    store_justified_epoch_equal_zero=store_je_eq_zero,
+                                                    block_voting_source_epoch_equal_store_justified_epoch=block_vse_eq_store_je,
+                                                    block_voting_source_epoch_plus_two_greater_or_equal_current_epoch=block_vse_plus_two_ge_curr_e,
+                                                    block_is_leaf=block_is_leaf,
+                                                    nr_solutions=nr_solutions)
+                    print('\n\n')
+                    print(['store_je_eq_zero=' + str(store_je_eq_zero),
+                            'block_vse_eq_store_je=' + str(block_vse_eq_store_je),
+                            'block_vse_plus_two_ge_curr_e=' + str(block_vse_plus_two_ge_curr_e),
+                            'block_is_leaf=' + str(block_is_leaf)])
+                    for r in results:
+                        print(r)
+
+                    solutions.extend(results)
+    
+    return solutions
+
+
 def _create_providers(forks: Iterable[SpecForkName],
         presets: Iterable[PresetBaseName],
         debug: bool,
         initial_seed: int,
-        anchor_epoch: int,
+        solutions,
         number_of_variations: int) -> Iterable[TestProvider]:
     def prepare_fn() -> None:
         bls.use_milagro()
@@ -73,29 +111,6 @@ def _create_providers(forks: Iterable[SpecForkName],
             rnd = random.Random(initial_seed)
             seeds = [rnd.randint(1, 10000) for _ in range(number_of_variations)]
             seeds[0] = initial_seed
-
-        solutions = []
-
-        for store_je_eq_zero in [True, False]:
-            for block_vse_eq_store_je in [True, False]:
-                for block_vse_plus_two_ge_curr_e in [True, False]:
-                    for block_is_leaf in [True, False]:
-                        if store_je_eq_zero and not block_vse_eq_store_je:
-                            continue
-                        results = _find_model_solutions(anchor_epoch=0 if store_je_eq_zero else anchor_epoch,
-                                                        store_justified_epoch_equal_zero=store_je_eq_zero,
-                                                        block_voting_source_epoch_equal_store_justified_epoch=block_vse_eq_store_je,
-                                                        block_voting_source_epoch_plus_two_greater_or_equal_current_epoch=block_vse_plus_two_ge_curr_e,
-                                                        block_is_leaf=block_is_leaf)
-                        print('\n\n')
-                        print(['store_je_eq_zero=' + str(store_je_eq_zero),
-                               'block_vse_eq_store_je=' + str(block_vse_eq_store_je),
-                               'block_vse_plus_two_ge_curr_e=' + str(block_vse_plus_two_ge_curr_e),
-                               'block_is_leaf=' + str(block_is_leaf)])
-                        for r in results:
-                            print(r)
-
-                        solutions = solutions + results
 
         for i, solution in enumerate(solutions):
             for seed in seeds:
@@ -156,14 +171,35 @@ if __name__ == "__main__":
         required=False,
         help='Anchor epoch'
     )
+    arg_parser.add_argument(
+        '--fc-gen-nr-solutions',
+        dest='fc_gen_nr_solutions',
+        default=5,
+        type=int,
+        required=False,
+        help='Number of solutions per MiniZinc query'
+    )
+    arg_parser.add_argument(
+        '--fc-gen-instances-path',
+        dest='fc_gen_instances_path',
+        default=None,
+        type=str,
+        required=False,
+        help='Path to a file with pre-generated instances'
+    )
 
     args = arg_parser.parse_args()
+
+    if args.fc_gen_instances_path is not None:
+        solutions = _load_model_solutions(args.fc_gen_instances_path)
+    else:
+        solutions = _generate_model_solutions(args.fc_gen_anchor_epoch, args.fc_gen_nr_solutions)
 
     gen_runner.run_generator(GENERATOR_NAME,
                              _create_providers(forks=forks,
                                                presets=presets,
                                                debug=args.fc_gen_debug,
                                                initial_seed=args.fc_gen_seed,
-                                               anchor_epoch=args.fc_gen_anchor_epoch,
+                                               solutions=solutions,
                                                number_of_variations=args.fc_gen_variations),
                              arg_parser)
