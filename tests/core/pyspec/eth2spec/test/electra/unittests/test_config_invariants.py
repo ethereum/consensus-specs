82133085a1295e93394ebdf71df8f2f6e0962588
Alex Stokes <r.alex.stokes@gmail.com>
2024-04-16 15:29:55 -0600

    clarify pending partial withdrawals handling

--- tests/core/pyspec/eth2spec/test/electra/unittests/test_config_invariants.py
+++ tests/core/pyspec/eth2spec/test/electra/unittests/test_config_invariants.py
@@ -8,5 +8,8 @@ from eth2spec.test.context import (
 @with_electra_and_later
 @spec_test
 @single_phase
-def test_withdrawals(spec):
-    assert spec.MAX_WITHDRAWAL_REQUESTS_PER_PAYLOAD < spec.MAX_WITHDRAWALS_PER_PAYLOAD
+def test_processing_pending_partial_withdrawals(spec):
+    assert (
+        spec.MAX_PENDING_PARTIALS_PER_WITHDRAWALS_SWEEP
+        < spec.MAX_WITHDRAWALS_PER_PAYLOAD
+    )
