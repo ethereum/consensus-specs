ef019bef4038dbed07001c8e8bfa55d01be0e496
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-04-22 21:58:44 +0100

    comment out kzg methods

--- tests/core/pyspec/eth2spec/test/eip7594/unittests/polynomial_commitments/test_polynomial_commitments.py
+++ tests/core/pyspec/eth2spec/test/eip7594/unittests/polynomial_commitments/test_polynomial_commitments.py
@@ -28,88 +28,88 @@ def test_fft(spec):
     assert poly_coeff_inversed == poly_coeff
 
 
-@with_eip7594_and_later
-@spec_test
-@single_phase
-def test_verify_cell_proof(spec):
-    blob = get_sample_blob(spec)
-    commitment = spec.blob_to_kzg_commitment(blob)
-    cells, proofs = spec.compute_cells_and_proofs(blob)
+# @with_eip7594_and_later
+# @spec_test
+# @single_phase
+# def test_verify_cell_proof(spec):
+#     blob = get_sample_blob(spec)
+#     commitment = spec.blob_to_kzg_commitment(blob)
+#     cells, proofs = spec.compute_cells_and_proofs(blob)
 
-    cell_id = 0
-    assert spec.verify_cell_proof(commitment, cell_id, cells[cell_id], proofs[cell_id])
-    cell_id = 1
-    assert spec.verify_cell_proof(commitment, cell_id, cells[cell_id], proofs[cell_id])
-
-
-@with_eip7594_and_later
-@spec_test
-@single_phase
-def test_verify_cell_proof_batch(spec):
-    blob = get_sample_blob(spec)
-    commitment = spec.blob_to_kzg_commitment(blob)
-    cells, proofs = spec.compute_cells_and_proofs(blob)
-
-    assert len(cells) == len(proofs)
-
-    assert spec.verify_cell_proof_batch(
-        row_commitments_bytes=[commitment],
-        row_indices=[0, 0],
-        column_indices=[0, 4],
-        cells=[cells[0], cells[4]],
-        proofs_bytes=[proofs[0], proofs[4]],
-    )
+#     cell_id = 0
+#     assert spec.verify_cell_proof(commitment, cell_id, cells[cell_id], proofs[cell_id])
+#     cell_id = 1
+#     assert spec.verify_cell_proof(commitment, cell_id, cells[cell_id], proofs[cell_id])
 
 
-@with_eip7594_and_later
-@spec_test
-@single_phase
-def test_recover_all_cells(spec):
-    rng = random.Random(5566)
+# @with_eip7594_and_later
+# @spec_test
+# @single_phase
+# def test_verify_cell_proof_batch(spec):
+#     blob = get_sample_blob(spec)
+#     commitment = spec.blob_to_kzg_commitment(blob)
+#     cells, proofs = spec.compute_cells_and_proofs(blob)
 
-    # Number of samples we will be recovering from
-    N_SAMPLES = spec.CELLS_PER_EXT_BLOB // 2
+#     assert len(cells) == len(proofs)
 
-    # Get the data we will be working with
-    blob = get_sample_blob(spec)
-
-    # Extend data with Reed-Solomon and split the extended data in cells
-    cells = spec.compute_cells(blob)
-
-    # Compute the cells we will be recovering from
-    cell_ids = []
-    # First figure out just the indices of the cells
-    for i in range(N_SAMPLES):
-        j = rng.randint(0, spec.CELLS_PER_EXT_BLOB - 1)
-        while j in cell_ids:
-            j = rng.randint(0, spec.CELLS_PER_EXT_BLOB - 1)
-        cell_ids.append(j)
-    # Now the cells themselves
-    known_cells = [cells[cell_id] for cell_id in cell_ids]
-
-    # Recover all of the cells
-    recovered_cells = spec.recover_all_cells(cell_ids, known_cells)
-    recovered_data = [x for xs in recovered_cells for x in xs]
-
-    # Check that the original data match the non-extended portion of the recovered data
-    blob_byte_array = [b for b in blob]
-    assert blob_byte_array == recovered_data[:len(recovered_data) // 2]
-
-    # Check that the recovered cells match the original cells
-    assert cells == recovered_cells
+#     assert spec.verify_cell_proof_batch(
+#         row_commitments_bytes=[commitment],
+#         row_indices=[0, 0],
+#         column_indices=[0, 4],
+#         cells=[cells[0], cells[4]],
+#         proofs_bytes=[proofs[0], proofs[4]],
+#     )
 
 
-@with_eip7594_and_later
-@spec_test
-@single_phase
-def test_multiply_polynomial_degree_overflow(spec):
-    rng = random.Random(5566)
+# @with_eip7594_and_later
+# @spec_test
+# @single_phase
+# def test_recover_all_cells(spec):
+#     rng = random.Random(5566)
 
-    # Perform a legitimate-but-maxed-out polynomial multiplication
-    poly1_coeff = [rng.randint(0, BLS_MODULUS - 1) for _ in range(spec.FIELD_ELEMENTS_PER_BLOB)]
-    poly2_coeff = [rng.randint(0, BLS_MODULUS - 1) for _ in range(spec.FIELD_ELEMENTS_PER_BLOB)]
-    _ = spec.multiply_polynomialcoeff(poly1_coeff, poly2_coeff)
+#     # Number of samples we will be recovering from
+#     N_SAMPLES = spec.CELLS_PER_EXT_BLOB // 2
 
-    # Now overflow the degree by pumping the degree of one of the inputs by one
-    poly2_coeff = [rng.randint(0, BLS_MODULUS - 1) for _ in range(spec.FIELD_ELEMENTS_PER_BLOB + 1)]
-    expect_assertion_error(lambda: spec.multiply_polynomialcoeff(poly1_coeff, poly2_coeff))
+#     # Get the data we will be working with
+#     blob = get_sample_blob(spec)
+
+#     # Extend data with Reed-Solomon and split the extended data in cells
+#     cells = spec.compute_cells(blob)
+
+#     # Compute the cells we will be recovering from
+#     cell_ids = []
+#     # First figure out just the indices of the cells
+#     for i in range(N_SAMPLES):
+#         j = rng.randint(0, spec.CELLS_PER_EXT_BLOB - 1)
+#         while j in cell_ids:
+#             j = rng.randint(0, spec.CELLS_PER_EXT_BLOB - 1)
+#         cell_ids.append(j)
+#     # Now the cells themselves
+#     known_cells = [cells[cell_id] for cell_id in cell_ids]
+
+#     # Recover all of the cells
+#     recovered_cells = spec.recover_all_cells(cell_ids, known_cells)
+#     recovered_data = [x for xs in recovered_cells for x in xs]
+
+#     # Check that the original data match the non-extended portion of the recovered data
+#     blob_byte_array = [b for b in blob]
+#     assert blob_byte_array == recovered_data[:len(recovered_data) // 2]
+
+#     # Check that the recovered cells match the original cells
+#     assert cells == recovered_cells
+
+
+# @with_eip7594_and_later
+# @spec_test
+# @single_phase
+# def test_multiply_polynomial_degree_overflow(spec):
+#     rng = random.Random(5566)
+
+#     # Perform a legitimate-but-maxed-out polynomial multiplication
+#     poly1_coeff = [rng.randint(0, BLS_MODULUS - 1) for _ in range(spec.FIELD_ELEMENTS_PER_BLOB)]
+#     poly2_coeff = [rng.randint(0, BLS_MODULUS - 1) for _ in range(spec.FIELD_ELEMENTS_PER_BLOB)]
+#     _ = spec.multiply_polynomialcoeff(poly1_coeff, poly2_coeff)
+
+#     # Now overflow the degree by pumping the degree of one of the inputs by one
+#     poly2_coeff = [rng.randint(0, BLS_MODULUS - 1) for _ in range(spec.FIELD_ELEMENTS_PER_BLOB + 1)]
+#     expect_assertion_error(lambda: spec.multiply_polynomialcoeff(poly1_coeff, poly2_coeff))
