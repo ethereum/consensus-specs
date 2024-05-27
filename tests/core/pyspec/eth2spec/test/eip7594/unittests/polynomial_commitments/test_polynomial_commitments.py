3382f841958e88c53b876c042ccae7218cf22a1a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-27 23:03:51 +0100

    chore: add previous sanity check as a unit test

--- tests/core/pyspec/eth2spec/test/eip7594/unittests/polynomial_commitments/test_polynomial_commitments.py
+++ tests/core/pyspec/eth2spec/test/eip7594/unittests/polynomial_commitments/test_polynomial_commitments.py
@@ -31,6 +31,30 @@ def test_fft(spec):
 @with_eip7594_and_later
 @spec_test
 @single_phase
+def test_compute_vanishing_polynomial(spec):
+    rng = random.Random(5566)
+
+    num_missing_cells = rng.randint(0, spec.CELLS_PER_EXT_BLOB - 1)
+    # Get a unique list of `num_missing_cells` cell IDs
+    unique_missing_cell_ids = rng.sample(range(spec.CELLS_PER_EXT_BLOB), num_missing_cells)
+
+    zero_poly_coeff = spec.construct_vanishing_polynomial(unique_missing_cell_ids)
+    roots_of_unity = spec.compute_roots_of_unity(spec.FIELD_ELEMENTS_PER_EXT_BLOB)
+    zero_poly_eval = spec.fft_field(zero_poly_coeff, roots_of_unity)
+    zero_poly_eval_brp = spec.bit_reversal_permutation(zero_poly_eval)
+
+    for cell_id in range(spec.CELLS_PER_EXT_BLOB):
+        start = cell_id * spec.FIELD_ELEMENTS_PER_CELL
+        end = (cell_id + 1) * spec.FIELD_ELEMENTS_PER_CELL
+        if cell_id in unique_missing_cell_ids:
+            assert all(a == 0 for a in zero_poly_eval_brp[start:end])
+        else:  # cell_id in cell_ids
+            assert all(a != 0 for a in zero_poly_eval_brp[start:end])
+
+
+@with_eip7594_and_later
+@spec_test
+@single_phase
 def test_verify_cell_kzg_proof(spec):
     blob = get_sample_blob(spec)
     commitment = spec.blob_to_kzg_commitment(blob)
