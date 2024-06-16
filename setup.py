cf46b07ac4257f37849c322aeb5194128fd0cb48
Etan Kissling <etan@status.im>
2024-06-16 10:04:41 +0200

    Bump remerkleable to `v0.1.28`
    - https://github.com/protolambda/remerkleable/releases/tag/v0.1.28

--- setup.py
+++ setup.py
@@ -552,7 +552,7 @@ setup(
         "pycryptodome==3.15.0",
         "py_ecc==6.0.0",
         "milagro_bls_binding==1.9.0",
-        "remerkleable==0.1.27",
+        "remerkleable==0.1.28",
         "trie==2.0.2",
         RUAMEL_YAML_VERSION,
         "lru-dict==1.2.0",
