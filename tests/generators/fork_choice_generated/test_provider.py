7ea774a578276dbe1369b1e0d900996eded278b3
Alex Vlasov <avv49@mail.ru>
2024-06-20 03:52:43 +0400

    refactoring of mutated test generation

--- tests/generators/fork_choice_generated/test_provider.py
+++ tests/generators/fork_choice_generated/test_provider.py
@@ -13,7 +13,7 @@ from eth2spec.utils import bls
 from scheduler import MessageScheduler
 from instantiators.block_cover import yield_block_cover_test_case, yield_block_cover_test_data
 from instantiators.block_tree import yield_block_tree_test_case, yield_block_tree_test_data
-from instantiators.helpers import FCTestData
+from instantiators.helpers import FCTestData, make_events, yield_fork_choice_test_events
 from mutation_operators import mutate_test_vector
 import random
 
@@ -51,12 +51,14 @@ class PlainFCTestCase(TestCase):
         if mut_seed is None:
             return list(self.call_instantiator(test_data_only=False))
         else:
-            test_data = list(self.call_instantiator(test_data_only=True))
-            fc_test_case, events = parse_test_data(spec, test_data)
-            tv_, = list(mutate_test_vector(random.Random(mut_seed), events, 1, debug=self.debug))
-            mutated_tc = update_test_case(spec, fc_test_case, tv_)
+            test_data = list(self.call_instantiator(test_data_only=True))[0][2]
+            test_vector = events_to_test_vector(make_events(spec, test_data))
+            mutated_vector, = list(mutate_test_vector(random.Random(mut_seed), test_vector, 1, debug=self.debug))
             #mutated_tc.meta['mutation_seed'] = mut_seed
-            return mutated_tc.dump()
+
+            mutated_events = test_vector_to_events(mutated_vector)
+
+            return yield_test_parts(spec, test_data, mutated_vector)
     
     def plain_case_fn(self) -> Iterable[TestCasePart]:
         yield from self.call_instantiator(test_data_only=False)
@@ -86,92 +88,61 @@ class PlainFCTestCase(TestCase):
             raise ValueError(f'Unknown FC test kind {self.test_dna.kind}')
 
 
-@dataclass
-class FCTestCase:
-    meta: dict
-    anchor_block: object
-    anchor_state: object
-    blocks: dict
-    atts: dict
-    slashings: dict
-    steps: Optional[list]
-
-    def with_steps(self, steps):
-        return FCTestCase(self.meta, self.anchor_block, self.anchor_state, self.blocks, self.atts, self.slashings, steps)
-    
-    def dump(self):
-        for k,v in self.meta.items():
-            yield k, 'meta', v
-        yield 'anchor_state', 'ssz', self.anchor_state.encode_bytes()
-        yield 'anchor_block', 'ssz', self.anchor_block.encode_bytes()
-        for k,v in self.blocks.items():
-            yield k, 'ssz', v.encode_bytes()
-        for k,v in self.atts.items():
-            yield k, 'ssz', v.encode_bytes()
-        for k,v in self.slashings.items():
-            yield k, 'ssz', v.encode_bytes()
-        yield 'steps', 'data', self.steps
-
-
-def parse_test_data(spec, _test_data) -> Tuple[FCTestCase, list[Any]]:
-    (elem_0, elem_1, elem_2), = _test_data
-    assert elem_1 == 'data' and elem_0 == 'test_data'
-
-    test_data: FCTestData = elem_2
-    meta = test_data.meta
-    anchor_state = test_data.anchor_state
-    anchor_block = test_data.anchor_block
-    blocks = {}
-    atts = {}
-    slashings = {}
-    events = []
-
-    store = spec.get_forkchoice_store(test_data.anchor_state, test_data.anchor_block)
-
-    def get_time(slot):
-        return int(slot * spec.config.SECONDS_PER_SLOT + store.genesis_time)
-
-    for b in test_data.blocks:
-        signed_block = b.payload
-        event_id = get_block_file_name(signed_block)
-        blocks[event_id] = signed_block
-        events.append((get_time(signed_block.message.slot), ('block', event_id)))
-    
-    for a in test_data.atts:
-        attestation = a.payload
-        event_id = get_attestation_file_name(attestation)
-        atts[event_id] = attestation
-        events.append((get_time(attestation.data.slot + 1), ('attestation', event_id)))
-    
-    for s in test_data.slashings:
-        slashing = s.payload
-        event_id = get_attester_slashing_file_name(slashing)
-        effective_slot = max(slashing.attestation_1.data.slot, slashing.attestation_2.data.slot) + 1
-        slashings[event_id] = slashing
-        events.append((get_time(effective_slot), ('attester_slashing', event_id)))
-
-    def get_key(event):
-        time, (event_type, _) = event
-        if event_type == 'block':
-            prio = 2
-        elif event_type == 'attestation':
-            prio = 0
+def events_to_test_vector(events) -> list[Any]:
+    test_vector = []
+    current_time = None
+    for event in events:
+        event_kind, data, _  = event
+        if event_kind == 'tick':
+            current_time = data
         else:
-            assert event_type == 'attester_slashing'
-            prio = 1
-        return (time, prio)
-    
-    events = sorted(events, key=get_key)
+            if event_kind == 'block':
+                event_id = data
+            elif event_kind == 'attestation':
+                event_id = data
+            elif event_kind == 'attester_slashing':
+                event_id = data
+            else:
+                assert False, event_kind
+            test_vector.append((current_time, (event_kind, event_id)))
+    return test_vector
 
-    return FCTestCase(meta, anchor_block, anchor_state, blocks, atts, slashings, []), events
+
+def test_vector_to_events(test_vector):
+    events = []
+    current_time = None
+    for time, (event_kind, data) in test_vector:
+        if time != current_time:
+            current_time = time
+            events.append(('tick', time, None))
+        events.append((event_kind, data, None))
+    return events
 
 
-def update_test_case(spec, fc_test_case: FCTestCase, events):
+def yield_test_parts(spec, test_data: FCTestData, events):
     old_bls_state = bls.bls_active
     bls.bls_active = False
     try:
-        anchor_state = fc_test_case.anchor_state
-        anchor_block = fc_test_case.anchor_block
+        for k,v in test_data.meta.items():
+            yield k, 'meta', v
+        
+        yield 'anchor_state', 'ssz', test_data.anchor_state.encode_bytes()
+        yield 'anchor_block', 'ssz', test_data.anchor_block.encode_bytes()
+
+        for message in test_data.blocks:
+            block = message.payload
+            yield get_block_file_name(block), 'ssz', block.encode_bytes()
+        
+        for message in test_data.atts:
+            attestation = message.payload
+            yield get_attestation_file_name(attestation), 'ssz', attestation.encode_bytes()
+        
+        for message in test_data.slashings:
+            attester_slashing = message.payload
+            yield get_attester_slashing_file_name(attester_slashing), 'ssz', attester_slashing.encode_bytes()
+        
+        anchor_state = test_data.anchor_state
+        anchor_block = test_data.anchor_block
         store = spec.get_forkchoice_store(anchor_state, anchor_block)
         test_steps = []
         scheduler = MessageScheduler(spec, anchor_state, anchor_block)
@@ -184,21 +155,18 @@ def update_test_case(spec, fc_test_case: FCTestCase, events):
             output_store_checks(spec, store, test_steps)
 
             if kind == 'block':
-                block_id = event
-                sb = fc_test_case.blocks[block_id]
-                valid = scheduler.process_block(sb)
+                block_id = get_block_file_name(event)
+                valid = scheduler.process_block(event)
                 test_steps.append({'block': block_id, 'valid': valid})
                 output_store_checks(spec, store, test_steps)
             elif kind == 'attestation':
-                att_id = event
-                att = fc_test_case.atts[att_id]
-                valid = scheduler.process_attestation(att, is_from_block=False)
+                att_id = get_attestation_file_name(event)
+                valid = scheduler.process_attestation(event, is_from_block=False)
                 test_steps.append({'attestation': att_id, 'valid': valid})
                 output_store_checks(spec, store, test_steps)
             elif kind == 'attester_slashing':
-                slashing_id = event
-                slashing = fc_test_case.slashings[slashing_id]
-                valid = scheduler.process_slashing(slashing)
+                slashing_id = get_attester_slashing_file_name(event)
+                valid = scheduler.process_slashing(event)
                 test_steps.append({'attester_slashing': slashing_id, 'valid': valid})
                 output_store_checks(spec, store, test_steps)
             else:
@@ -207,7 +175,7 @@ def update_test_case(spec, fc_test_case: FCTestCase, events):
         # on_tick_and_append_step(spec, store, next_slot_time, test_steps, checks_with_viable_for_head_weights=True)
         on_tick_and_append_step(spec, store, next_slot_time, test_steps)
 
-        return fc_test_case.with_steps(test_steps)
+        yield 'steps', 'data', test_steps
     finally:
         bls.bls_active = old_bls_state
 
