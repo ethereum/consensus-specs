96f29cf28596a19a32ae0661e13fad1ee8a39d9d
Alex Vlasov <avv49@mail.ru>
2024-06-19 07:24:38 +0400

    Code reorg: make helper methods

--- tests/generators/fork_choice_generated/test_gen.py
+++ tests/generators/fork_choice_generated/test_gen.py
@@ -10,10 +10,49 @@ from instance_generator import (
 from test_provider import GENERATOR_NAME, create_providers
 
 
-yaml = YAML(typ='safe')
+def run_test_group(test_name, test_type, instances_path,
+                   initial_seed, nr_variations, nr_mutations,
+                   with_attester_slashings, with_invalid_messages,
+                   debug=False, arg_parser=None):
+    if test_type == 'block_tree':
+        solutions = _load_block_tree_instances(instances_path)
+        if not with_attester_slashings and not with_invalid_messages:
+            test_kind = 'block_tree_test'
+        elif with_attester_slashings and not with_invalid_messages:
+            test_kind = 'attester_slashing_test'
+        elif not with_attester_slashings and with_invalid_messages:
+            test_kind = 'invalid_message_test'
+        else:
+            test_kind = 'attestet_slashing_and_invalid_message_test'
+    elif test_type == 'block_cover':
+        solutions = _load_block_cover_instances(instances_path)
+        test_kind = 'block_cover_test'
+    else:
+        raise ValueError(f'Unsupported test type: {test_type}')
+    
+    providers = create_providers(test_name, forks, presets, debug, initial_seed,
+                                    solutions, nr_variations, nr_mutations, test_kind)
+    gen_runner.run_generator(GENERATOR_NAME, providers, arg_parser)
 
 
-if __name__ == "__main__":
+def run_test_config(test_gen_config, debug=False, arg_parser=None):
+    for test_name, params in test_gen_config.items():
+        print(test_name)
+        test_type = params['test_type']
+        instances_path = params['instances']
+        initial_seed = params['seed']
+        nr_variations = params['nr_variations']
+        nr_mutations = params['nr_mutations']
+        with_attester_slashings = params.get('with_attester_slashings', False)
+        with_invalid_messages = params.get('with_invalid_messages', False)
+
+        run_test_group(test_name, test_type, instances_path,
+                       initial_seed, nr_variations, nr_mutations,
+                       with_attester_slashings, with_invalid_messages,
+                       debug=debug, arg_parser=arg_parser)
+
+
+def main():
     arg_parser = gen_runner.create_arg_parser(GENERATOR_NAME)
 
     arg_parser.add_argument(
@@ -35,37 +74,10 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
 
     with open(args.fc_gen_config, 'r') as f:
+        yaml = YAML(typ='safe')
         test_gen_config = yaml.load(f)
     
-    for test_name, params in test_gen_config.items():
-        print(test_name)
-        test_type = params['test_type']
-        instances_path = params['instances']
-        initial_seed = params['seed']
-        nr_variations = params['nr_variations']
-        nr_mutations = params['nr_mutations']
-        with_attester_slashings = params.get('with_attester_slashings', False)
-        with_invalid_messages = params.get('with_invalid_messages', False)
-        debug = args.fc_gen_debug
+    run_test_config(test_gen_config, debug = args.fc_gen_debug, arg_parser=arg_parser)
 
-        if test_type == 'block_tree':
-            solutions = _load_block_tree_instances(instances_path)
-            if not with_attester_slashings and not with_invalid_messages:
-                test_kind = 'block_tree_test'
-            elif with_attester_slashings and not with_invalid_messages:
-                test_kind = 'attester_slashing_test'
-            elif not with_attester_slashings and with_invalid_messages:
-                test_kind = 'invalid_message_test'
-            else:
-                test_kind = 'attestet_slashing_and_invalid_message_test'
-        elif test_type == 'block_cover':
-            solutions = _load_block_cover_instances(instances_path)
-            test_kind = 'block_cover_test'
-        else:
-            raise ValueError(f'Unsupported test type: {test_type}')
-        
-        providers = create_providers(test_name, forks, presets, debug, initial_seed,
-                                      solutions, nr_variations, nr_mutations, test_kind)
-        gen_runner.run_generator(GENERATOR_NAME, providers, arg_parser)
-
-
+if __name__ == "__main__":
+    main()
