cf688131519ff4c9b38710e63ac70ddb728b4c88
Barnabas Busa <busa.barnabas@gmail.com>
2024-08-28 11:16:28 +0200

    update tests

--- tests/core/pyspec/eth2spec/test/eip7594/unittests/test_custody.py
+++ tests/core/pyspec/eth2spec/test/eip7594/unittests/test_custody.py
@@ -11,6 +11,7 @@ def run_get_custody_columns(spec, peer_count, custody_subnet_count):
     assignments = [spec.get_custody_columns(node_id, custody_subnet_count) for node_id in range(peer_count)]
 
     columns_per_subnet = spec.config.NUMBER_OF_COLUMNS // spec.config.DATA_COLUMN_SIDECAR_SUBNET_COUNT
+    assert custody_subnet_count <= spec.config.DATA_COLUMN_SIDECAR_SUBNET_COUNT
     for assignment in assignments:
         assert len(assignment) == custody_subnet_count * columns_per_subnet
         assert len(assignment) == len(set(assignment))
@@ -57,5 +58,5 @@ def test_get_custody_columns_custody_size_more_than_number_of_columns(spec):
 @with_eip7594_and_later
 @spec_test
 @single_phase
-def test_custody_subnet_count_int_bitlength(spec, custody_subnet_count):
-    assert uint8(custody_subnet_count) == custody_subnet_count
+def test_custody_subnet_count_int_bitlength(spec):
+    assert spec.config.DATA_COLUMN_SIDECAR_SUBNET_COUNT == uint8(spec.config.DATA_COLUMN_SIDECAR_SUBNET_COUNT)
