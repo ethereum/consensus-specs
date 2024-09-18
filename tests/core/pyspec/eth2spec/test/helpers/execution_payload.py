cb99c5f3023a60511d37f21f57cdf22ff86cc8a4
Etan Kissling <etan@nimbus.team>
2024-09-18 16:01:55 +0200

    Ensure EL block hash is updated when beacon parent root is overridden (#3881)

--- tests/core/pyspec/eth2spec/test/helpers/execution_payload.py
+++ tests/core/pyspec/eth2spec/test/helpers/execution_payload.py
@@ -191,17 +191,19 @@ def get_consolidation_request_rlp_bytes(consolidation_request):
     return b"\x02" + encode(values, sedes)
 
 
-def compute_el_block_hash(spec, payload, pre_state):
+def compute_el_block_hash_with_parent_root(spec, payload, parent_beacon_block_root):
+    if payload == spec.ExecutionPayload():
+        return spec.Hash32()
+
     transactions_trie_root = compute_trie_root_from_indexed_data(payload.transactions)
 
     withdrawals_trie_root = None
-    parent_beacon_block_root = None
 
     if is_post_capella(spec):
         withdrawals_encoded = [get_withdrawal_rlp(withdrawal) for withdrawal in payload.withdrawals]
         withdrawals_trie_root = compute_trie_root_from_indexed_data(withdrawals_encoded)
-    if is_post_deneb(spec):
-        parent_beacon_block_root = pre_state.latest_block_header.hash_tree_root()
+    if not is_post_deneb(spec):
+        parent_beacon_block_root = None
 
     payload_header = get_execution_payload_header(spec, payload)
 
@@ -212,6 +214,24 @@ def compute_el_block_hash(spec, payload, pre_state):
         withdrawals_trie_root,
         parent_beacon_block_root,
     )
+
+
+def compute_el_block_hash(spec, payload, pre_state):
+    parent_beacon_block_root = None
+
+    if is_post_deneb(spec):
+        previous_block_header = pre_state.latest_block_header.copy()
+        if previous_block_header.state_root == spec.Root():
+            previous_block_header.state_root = pre_state.hash_tree_root()
+        parent_beacon_block_root = previous_block_header.hash_tree_root()
+
+    return compute_el_block_hash_with_parent_root(
+        spec, payload, parent_beacon_block_root)
+
+
+def compute_el_block_hash_for_block(spec, block):
+    return compute_el_block_hash_with_parent_root(
+        spec, block.body.execution_payload, block.parent_root)
 
 
 def build_empty_post_eip7732_execution_payload_header(spec, state):
