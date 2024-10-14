179cde6780c677a1293c4f075ac749787715aaca
Justin Traglia <95511699+jtraglia@users.noreply.github.com>
2024-10-14 10:42:57 -0500

    Add new `check_mods` function for generators (#3970)
    * Add new check_mods function for generators
    
    * Use single qoutes for consistency
    
    * Add [ERROR] to the exception & update readme
    
    * Fix typos
    
    * Fix lint

--- tests/generators/networking/main.py
+++ tests/generators/networking/main.py
@@ -1,6 +1,6 @@
 
 from eth2spec.test.helpers.constants import EIP7594
-from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators
+from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators, check_mods
 
 
 if __name__ == "__main__":
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     all_mods = {
         EIP7594: eip7594_mods
     }
+    check_mods(all_mods, "networking")
 
     run_state_test_generators(runner_name="networking", all_mods=all_mods)
