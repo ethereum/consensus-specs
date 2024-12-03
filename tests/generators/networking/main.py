91bf0290de36b668c1079a95ef38bc37eb91d4b4
Justin Traglia <jtraglia@pm.me>
2024-12-03 15:52:13 -0600

    Fix networking generator modules

--- tests/generators/networking/main.py
+++ tests/generators/networking/main.py
@@ -5,8 +5,7 @@ from eth2spec.gen_helpers.gen_from_tests.gen import run_state_test_generators, c
 
 if __name__ == "__main__":
     eip7594_mods = {key: 'eth2spec.test.eip7594.networking.test_' + key for key in [
-        'compute_columns_for_custody_group',
-        'get_custody_groups',
+        'get_custody_columns',
     ]}
     all_mods = {
         EIP7594: eip7594_mods
