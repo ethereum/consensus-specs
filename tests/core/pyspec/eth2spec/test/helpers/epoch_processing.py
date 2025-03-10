c2b14f6c67e4fe134d3c24d25f89c83cb376a2c1
Leo Lara <leo@leolara.me>
2025-03-10 19:26:49 +0700

    Add to epoch processing tests generation states before and after full epoch processing

--- tests/core/pyspec/eth2spec/test/helpers/epoch_processing.py
+++ tests/core/pyspec/eth2spec/test/helpers/epoch_processing.py
@@ -40,18 +40,12 @@ def get_process_calls(spec):
     ]
 
 
-def run_epoch_processing_to(spec, state, process_name: str):
+def run_epoch_processing_to(spec, state, process_name: str, disable_slots_processing=False):
     """
     Processes to the next epoch transition, up to, but not including, the sub-transition named ``process_name``
     """
-    slot = state.slot + (spec.SLOTS_PER_EPOCH - state.slot % spec.SLOTS_PER_EPOCH)
-
-    # transition state to slot before epoch state transition
-    if state.slot < slot - 1:
-        spec.process_slots(state, slot - 1)
-
-    # start transitioning, do one slot update before the epoch itself.
-    spec.process_slot(state)
+    if not disable_slots_processing:
+        run_process_slots_up_to_epoch_boundary(spec, state)
 
     # process components of epoch transition before final-updates
     for name in get_process_calls(spec):
@@ -62,13 +56,53 @@ def run_epoch_processing_to(spec, state, process_name: str):
             getattr(spec, name)(state)
 
 
+def run_process_slots_up_to_epoch_boundary(spec, state):
+    """
+    Processes slots until the next epoch transition
+    """
+    slot = state.slot + (spec.SLOTS_PER_EPOCH - state.slot %
+                         spec.SLOTS_PER_EPOCH)
+
+    # transition state to slot before epoch state transition
+    if state.slot < slot - 1:
+        spec.process_slots(state, slot - 1)
+
+    # start transitioning, do one slot update before the epoch itself.
+    spec.process_slot(state)
+
+
+def run_epoch_processing_from(spec, state, process_name: str):
+    """
+    Processes to the next epoch transition, fom, but not including, the sub-transition named ``process_name``
+    """
+    assert (state.slot + 1) % spec.SLOTS_PER_EPOCH == 0
+
+    procesing = False
+    for name in get_process_calls(spec):
+        if name == process_name:
+            procesing = True
+            continue
+        # only run when present. Later phases introduce more to the epoch-processing.
+        if procesing and hasattr(spec, name):
+            getattr(spec, name)(state)
+
+
 def run_epoch_processing_with(spec, state, process_name: str):
     """
     Processes to the next epoch transition, up to and including the sub-transition named ``process_name``
       - pre-state ('pre'), state before calling ``process_name``
       - post-state ('post'), state after calling ``process_name``
+      - pre-full-state ('pre_full'), state before epoch transition
+      - post-full-state ('post_full'), state after epoch transition
+    The state passed by reference will be modified to be the ``process_name``post state.
     """
-    run_epoch_processing_to(spec, state, process_name)
+    run_process_slots_up_to_epoch_boundary(spec, state)
+    yield "pre_full", state
+    run_epoch_processing_to(spec, state, process_name,
+                            disable_slots_processing=True)
     yield 'pre', state
     getattr(spec, process_name)(state)
     yield 'post', state
+    continue_state = state.copy()
+    run_epoch_processing_from(spec, continue_state, process_name)
+    yield 'post_full', continue_state
