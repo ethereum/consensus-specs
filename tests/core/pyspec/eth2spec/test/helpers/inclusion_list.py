3824af8de2095727f94132e93748f9ff2ac47e65
Jihoon Song <jihoonsong@users.noreply.github.com>
2025-06-01 19:17:31 +0900

    Add EIP-7805 tests for `process_execution_payload` (WIP)

--- tests/core/pyspec/eth2spec/test/helpers/inclusion_list.py
+++ tests/core/pyspec/eth2spec/test/helpers/inclusion_list.py
@@ -1,3 +1,5 @@
+import random
+
 from eth2spec.utils.ssz.ssz_impl import hash_tree_root
 from eth2spec.utils.ssz.ssz_typing import List
 
@@ -28,3 +30,70 @@ def get_empty_inclusion_list(spec, state, slot=None, validator_index=None):
     empty_inclusion_list.transactions = []
 
     return empty_inclusion_list
+
+
+def get_sample_inclusion_list(
+    spec,
+    state,
+    rng=random.Random(7805),
+    slot=None,
+    validator_index=None,
+    max_transaction_count=1,
+    is_valid_inclusion_list=True,
+):
+    """
+    Build sample inclusion list for ``slot``. Slot must be greater than or equal to the current slot in ``state``.
+    """
+    inclusion_list = get_empty_inclusion_list(spec, state, slot, validator_index)
+    inclusion_list.transactions = get_sample_inclusion_list_transactions(
+        spec, state, rng, max_transaction_count, is_valid_inclusion_list
+    )
+
+    return inclusion_list
+
+
+def get_sample_inclusion_list_transactions(
+    spec,
+    state,
+    rng=random.Random(7805),
+    max_transaction_count=1,
+    is_valid_inclusion_list=True,
+):
+    """
+    Build sample inclusion list transactions for ``slot``. Slot must be greater than or equal to the current slot in ``state``.
+    """
+    transaction_size = 200
+    transaction_count = min(
+        max_transaction_count, spec.config.MAX_BYTES_PER_INCLUSION_LIST // transaction_size
+    )
+    transactions = [spec.Transaction(b"\x99" * transaction_size) for _ in range(transaction_count)]
+
+    return transactions
+
+
+def run_with_inclusion_list(spec, inclusion_list, func):
+    """
+    This helper runs the given ``func`` with monkeypatched ``retrieve_inclusion_list_transactions``
+    that returns ``inclusion_list.transactions``.
+    """
+
+    def retrieve_inclusion_list_transactions(state, slot):
+        return inclusion_list.transactions
+
+    retrieve_inclusion_list_transactions_backup = spec.retrieve_inclusion_list_transactions
+    spec.retrieve_inclusion_list_transactions = retrieve_inclusion_list_transactions
+
+    class AtomicBoolean:
+        value = False
+
+    is_called = AtomicBoolean()
+
+    def wrap(flag: AtomicBoolean):
+        yield from func()
+        flag.value = True
+
+    try:
+        yield from wrap(is_called)
+    finally:
+        spec.retrieve_inclusion_list_transactions = retrieve_inclusion_list_transactions_backup
+    assert is_called.value
