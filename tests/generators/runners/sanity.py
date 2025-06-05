ba1ec336bab70a4e20f7df4bf315b9bc337fc1ba
Justin Traglia <jtraglia@pm.me>
2025-06-04 21:07:09 -0500

    Alias effective_balance_increase_changes_lookahead to blocks handler

--- tests/generators/runners/sanity.py
+++ tests/generators/runners/sanity.py
@@ -8,6 +8,8 @@ def handler_name_fn(mod):
     handler_name = mod.split(".")[-1]
     if handler_name == "test_deposit_transition":
         return "blocks"
+    if handler_name == "test_effective_balance_increase_changes_lookahead":
+        return "blocks"
     return handler_name.replace("test_", "")
 
 
