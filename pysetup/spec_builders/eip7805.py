d14a25b83f3ffd30b334dcbc9c2166b9a5c5b14d
JihoonSong <jihoonsong@users.noreply.github.com>
2025-07-17 03:12:00 +0900

    Resolve TODOs and Improve EIP-7805 (FOCIL) Spec (#4351)
    This PR resolves TODOs and improves the honest validator section of
    EIP-7805 spec. It contains minimal test changes and rest of tests will
    be added by the subsequent PR.

--- pysetup/spec_builders/eip7805.py
+++ pysetup/spec_builders/eip7805.py
@@ -12,6 +12,14 @@ from eth2spec.electra import {preset_name} as electra
 """
 
     @classmethod
+    def sundry_functions(cls) -> str:
+        return """
+def cached_or_new_inclusion_list_store() -> InclusionListStore:
+    # pylint: disable=unused-argument
+    return InclusionListStore()
+"""
+
+    @classmethod
     def execution_engine_cls(cls) -> str:
         return """
 class NoopExecutionEngine(ExecutionEngine):
@@ -19,8 +27,7 @@ class NoopExecutionEngine(ExecutionEngine):
     def notify_new_payload(self: ExecutionEngine,
                            execution_payload: ExecutionPayload,
                            parent_beacon_block_root: Root,
-                           execution_requests_list: Sequence[bytes],
-                           inclusion_list_transactions: Sequence[Transaction]) -> bool:
+                           execution_requests_list: Sequence[bytes]) -> bool:
         return True
 
     def notify_forkchoice_updated(self: ExecutionEngine,
@@ -37,8 +44,7 @@ class NoopExecutionEngine(ExecutionEngine):
     def is_valid_block_hash(self: ExecutionEngine,
                             execution_payload: ExecutionPayload,
                             parent_beacon_block_root: Root,
-                            execution_requests_list: Sequence[bytes],
-                            inclusion_list_transactions: Sequence[Transaction]) -> bool:
+                            execution_requests_list: Sequence[bytes]) -> bool:
         return True
 
     def is_valid_versioned_hashes(self: ExecutionEngine, new_payload_request: NewPayloadRequest) -> bool:
@@ -46,6 +52,15 @@ class NoopExecutionEngine(ExecutionEngine):
 
     def verify_and_notify_new_payload(self: ExecutionEngine,
                                       new_payload_request: NewPayloadRequest) -> bool:
+        return True
+
+    def get_inclusion_list(self: ExecutionEngine) -> GetInclusionListResponse:
+        # pylint: disable=unused-argument
+        raise NotImplementedError("no default inclusion list production")
+
+    def is_inclusion_list_satisfied(self: ExecutionEngine,
+                                    execution_payload: ExecutionPayload,
+                                    inclusion_list_transactions: Sequence[Transaction]) -> bool:
         return True
 
 
