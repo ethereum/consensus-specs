763ac1b153a53a7d51c7b2e32ee38656e054043d
Etan Kissling <etan@nimbus.team>
2025-08-26 17:15:33 +0200

    Extend tests with version mismatches of slightly different structs

--- tests/generators/runners/ssz_generic_cases/ssz_progressive_container.py
+++ tests/generators/runners/ssz_generic_cases/ssz_progressive_container.py
@@ -1,6 +1,8 @@
 from collections.abc import Sequence
 from random import Random
 
+from eth2spec.debug.random_value import RandomizationMode
+from eth2spec.utils.ssz.ssz_impl import deserialize, serialize
 from eth2spec.utils.ssz.ssz_typing import (
     byte,
     List,
@@ -13,11 +15,13 @@ from eth2spec.utils.ssz.ssz_typing import (
 )
 
 from .ssz_container import (
+    container_case_fn,
     invalid_container_cases,
     SmallTestStruct,
     valid_container_cases,
     VarTestStruct,
 )
+from .ssz_test_case import invalid_test_case
 
 
 class ProgressiveSingleFieldContainerTestStruct(ProgressiveContainer(active_fields=[1])):
@@ -49,11 +53,94 @@ class ProgressiveComplexTestStruct(
     H: ProgressiveList[ProgressiveVarTestStruct]
 
 
+class ModifiedTestStruct1(ProgressiveContainer(active_fields=[1, 1])):
+    A: byte
+    X: byte
+
+
+class ModifiedTestStruct2(ProgressiveContainer(active_fields=[1, 0, 1])):
+    A: byte
+    B: List[uint16, 123]
+
+
+class ModifiedTestStruct3(ProgressiveContainer(active_fields=[1, 1, 1])):
+    A: byte
+    X: byte
+    B: List[uint16, 123]
+
+
+class ModifiedTestStruct4(ProgressiveContainer(active_fields=[0, 0, 1, 0, 1])):
+    B: List[uint16, 123]
+    C: ProgressiveBitlist
+
+
+class ModifiedTestStruct5(ProgressiveContainer(active_fields=[1, 0, 0, 0, 1])):
+    A: byte
+    C: ProgressiveBitlist
+
+
+class ModifiedTestStruct6(ProgressiveContainer(active_fields=[1, 1, 1, 0, 1, 0, 0, 0, 1])):
+    A: byte
+    X: byte
+    B: List[uint16, 123]
+    C: ProgressiveBitlist
+    D: ProgressiveList[uint64]
+
+
+class ModifiedTestStruct7(ProgressiveContainer(active_fields=[1, 0, 1, 0, 0, 0, 0, 0, 1])):
+    A: byte
+    B: List[uint16, 123]
+    D: ProgressiveList[uint64]
+
+
+class ModifiedTestStruct8(
+    ProgressiveContainer(
+        active_fields=[1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1]
+    )
+):
+    A: byte
+    X: byte
+    B: List[uint16, 123]
+    C: ProgressiveBitlist
+    D: ProgressiveList[uint64]
+    E: ProgressiveList[SmallTestStruct]
+    F: ProgressiveList[ProgressiveList[VarTestStruct]]
+    G: List[ProgressiveSingleFieldContainerTestStruct, 10]
+    H: ProgressiveList[ProgressiveVarTestStruct]
+
+
+class ModifiedTestStruct9(
+    ProgressiveContainer(
+        active_fields=[1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1]
+    )
+):
+    A: byte
+    B: List[uint16, 123]
+    C: ProgressiveBitlist
+    D: ProgressiveList[uint64]
+    F: ProgressiveList[ProgressiveList[VarTestStruct]]
+    G: List[ProgressiveSingleFieldContainerTestStruct, 10]
+    H: ProgressiveList[ProgressiveVarTestStruct]
+
+
 PRESET_PROGRESSIVE_CONTAINERS: dict[str, tuple[type[View], Sequence[int]]] = {
     "ProgressiveSingleFieldContainerTestStruct": (ProgressiveSingleFieldContainerTestStruct, []),
     "ProgressiveSingleListContainerTestStruct": (ProgressiveSingleListContainerTestStruct, [0]),
     "ProgressiveVarTestStruct": (ProgressiveVarTestStruct, [1, 5]),
     "ProgressiveComplexTestStruct": (ProgressiveComplexTestStruct, [1, 5, 9, 13, 17, 21, 25]),
+}
+
+
+MODIFIED_PROGRESSIVE_CONTIANERS: Sequence[type[View]] = {
+    ModifiedTestStruct1,
+    ModifiedTestStruct2,
+    ModifiedTestStruct3,
+    ModifiedTestStruct4,
+    ModifiedTestStruct5,
+    ModifiedTestStruct6,
+    ModifiedTestStruct7,
+    ModifiedTestStruct8,
+    ModifiedTestStruct9,
 }
 
 
@@ -67,3 +154,19 @@ def invalid_cases():
     rng = Random(1234)
     for name, (typ, offsets) in PRESET_PROGRESSIVE_CONTAINERS.items():
         yield from invalid_container_cases(rng, name, typ, offsets)
+
+        for mode in [
+            RandomizationMode.mode_random,
+            RandomizationMode.mode_nil_count,
+            RandomizationMode.mode_one_count,
+            RandomizationMode.mode_max_count,
+        ]:
+            for i, modded_typ in enumerate(MODIFIED_PROGRESSIVE_CONTIANERS):
+                serialized = serialize(container_case_fn(rng, mode, modded_typ))
+                try:
+                    _ = deserialize(typ, serialized)
+                except Exception:
+                    yield (
+                        f"{name}_{mode.to_name()}_modded_{i}",
+                        invalid_test_case(lambda serialized=serialized: serialized),
+                    )
