2ed37488618419eb18b6f00be558bcf6c3fce95d
Leo Lara <leo@leolara.me>
2025-09-03 08:58:03 +0000

    lint

--- tests/infra/generator.py
+++ tests/infra/generator.py
@@ -6,9 +6,11 @@ def only_generator(func):
     Decorator that only calls the decorated function when test vectors are being generated.
     It should be used before vector_test decorator and other decorators that use it like spec_test.
     """
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if kwargs.get('generator_mode', False):
+        if kwargs.get("generator_mode", False):
             return func(*args, **kwargs)
         return None
+
     return wrapper
--- tests/infra/test_generator.py
+++ tests/infra/test_generator.py
@@ -1,4 +1,3 @@
-import pytest
 from tests.infra.generator import only_generator
 
 
@@ -18,46 +17,45 @@ def function_with_args_and_kwargs(*args, **kwargs):
 
 
 class TestOnlyGeneratorDecorator:
-    
     def test_function_called_when_generator_mode_true(self):
         result = sample_function(5, 3, generator_mode=True)
         assert result == 8
-    
+
     def test_function_not_called_when_generator_mode_false(self):
         result = sample_function(5, 3, generator_mode=False)
         assert result is None
-    
+
     def test_function_not_called_when_generator_mode_missing(self):
         result = sample_function(5, 3)
         assert result is None
-    
+
     def test_function_with_default_kwargs_generator_mode_true(self):
         result = function_with_kwargs(5, generator_mode=True)
         assert result == 50
-    
+
     def test_function_with_custom_kwargs_generator_mode_true(self):
         result = function_with_kwargs(3, b=7, generator_mode=True)
         assert result == 21
-    
+
     def test_function_with_custom_kwargs_generator_mode_false(self):
         result = function_with_kwargs(3, b=7, generator_mode=False)
         assert result is None
-    
+
     def test_function_with_args_and_kwargs_generator_mode_true(self):
         result = function_with_args_and_kwargs(1, 2, 3, x=4, y=5, generator_mode=True)
         assert result == 16  # sum(1,2,3) + sum(4,5,1) = 6 + 10 = 16 (generator_mode=True adds 1)
-    
+
     def test_function_with_args_and_kwargs_generator_mode_false(self):
         result = function_with_args_and_kwargs(1, 2, 3, x=4, y=5, generator_mode=False)
         assert result is None
-    
+
     def test_function_preserves_function_name(self):
-        assert sample_function.__name__ == 'sample_function'
-    
+        assert sample_function.__name__ == "sample_function"
+
     def test_function_preserves_docstring(self):
         @only_generator
         def documented_function(generator_mode=False):
             """This is a test function."""
             return "result"
-        
+
         assert documented_function.__doc__ == "This is a test function."
