f493add164f5e59f04717336c517b57583a0ef73
Justin Traglia <jtraglia@pm.me>
2026-03-06 21:56:56 -0600

    Fix sampling config test

--- tests/core/pyspec/eth_consensus_specs/test/fulu/unittests/test_security.py
+++ tests/core/pyspec/eth_consensus_specs/test/fulu/unittests/test_security.py
@@ -2,7 +2,7 @@ from eth_consensus_specs.test.context import (
     single_phase,
     spec_test,
     with_fulu_and_later,
-    with_phases,
+    with_presets,
 )
 from eth_consensus_specs.test.helpers.constants import (
     MAINNET,
@@ -12,9 +12,9 @@ from eth_consensus_specs.test.helpers.constants import (
 @with_fulu_and_later
 @spec_test
 @single_phase
-@with_phases([MAINNET])
+@with_presets([MAINNET])
 def test_sampling_config(spec):
-    probability_of_unavailable = 2 ** (-int(spec.SAMPLES_PER_SLOT))
+    probability_of_unavailable = 2 ** (-int(spec.config.SAMPLES_PER_SLOT))
     # TODO: What is the security requirement?
     security_requirement = 0.01
     assert probability_of_unavailable <= security_requirement
@@ -24,7 +24,7 @@ def test_sampling_config(spec):
         * spec.BYTES_PER_FIELD_ELEMENT
         * spec.config.MAX_BLOBS_PER_BLOCK
     )
-    bytes_per_slot = column_size_in_bytes * spec.SAMPLES_PER_SLOT
+    bytes_per_slot = column_size_in_bytes * spec.config.SAMPLES_PER_SLOT
     # TODO: What is the bandwidth requirement?
     bandwidth_requirement = 10000  # bytes/s
     assert bytes_per_slot * 1000 // spec.config.SLOT_DURATION_MS < bandwidth_requirement
