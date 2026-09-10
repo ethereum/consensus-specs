44bbbeeeb9d4ab2f008b6b8414e7f5c997613d12
Justin Traglia <jtraglia@pm.me>
2026-09-10 12:56:49 -0500

    Remove tests for eip8148

--- tests/core/pyspec/eth_consensus_specs/test/helpers/constants.py
+++ tests/core/pyspec/eth_consensus_specs/test/helpers/constants.py
@@ -17,7 +17,6 @@ HEZE = SpecForkName("heze")
 
 # Experimental phases (not included in default "ALL_PHASES"):
 EIP8025 = SpecForkName("eip8025")
-EIP8148 = SpecForkName("eip8148")
 EIP8205 = SpecForkName("eip8205")
 EIP8321 = SpecForkName("eip8321")
 
@@ -38,7 +37,6 @@ ALL_PHASES = (
     HEZE,
     # Experimental patches
     EIP8025,
-    EIP8148,
     EIP8205,
     EIP8321,
 )
@@ -63,7 +61,6 @@ PREVIOUS_FORK_OF = {
     HEZE: GLOAS,
     # Experimental patches
     EIP8025: GLOAS,
-    EIP8148: HEZE,
     EIP8205: HEZE,
     EIP8321: HEZE,
 }
