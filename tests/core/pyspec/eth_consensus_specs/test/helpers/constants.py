97cbc3a1ee9126c265b882111a6d6fe24793912d
Justin Traglia <jtraglia@pm.me>
2026-09-15 10:30:58 -0500

    Remove changes to tests

--- tests/core/pyspec/eth_consensus_specs/test/helpers/constants.py
+++ tests/core/pyspec/eth_consensus_specs/test/helpers/constants.py
@@ -16,7 +16,6 @@ GLOAS = SpecForkName("gloas")
 HEZE = SpecForkName("heze")
 
 # Experimental phases (not included in default "ALL_PHASES"):
-EIP7716 = SpecForkName("eip7716")
 EIP8025 = SpecForkName("eip8025")
 EIP8148 = SpecForkName("eip8148")
 EIP8205 = SpecForkName("eip8205")
@@ -38,7 +37,6 @@ ALL_PHASES = (
     GLOAS,
     HEZE,
     # Experimental patches
-    EIP7716,
     EIP8025,
     EIP8148,
     EIP8205,
@@ -64,7 +62,6 @@ PREVIOUS_FORK_OF = {
     GLOAS: FULU,
     HEZE: GLOAS,
     # Experimental patches
-    EIP7716: HEZE,
     EIP8025: GLOAS,
     EIP8148: HEZE,
     EIP8205: HEZE,
